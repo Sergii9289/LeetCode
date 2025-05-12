d30c08c9b82dce6149f6b68d73bce9aaac182810
Sergii <zx9289@gmail.com>
2025-05-12 20:38:14 +0300

    API TG Bot vacuumcleaned

--- TG_Bot_API/app/handlers.py
+++ TG_Bot_API/app/handlers.py
@@ -1,8 +1,7 @@
 from aiogram import F, Router
 from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart, Command
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
+from aiogram.filters import CommandStart
+
 
 import app.keyboards as kb
 import app.database.requests as rq
